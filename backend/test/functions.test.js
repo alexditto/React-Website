481b2e0834641d6b11058c875e997a40217c3d46
Alex Ditto <49873977+alexditto@users.noreply.github.com>
2021-12-27 23:00:49 -0600

    small commit

--- backend/test/functions.test.js
+++ backend/test/functions.test.js
@@ -15,3 +15,7 @@ test("Should be null", ()=>{
 test("Create new user", ()=>{
     expect(functions.createUser()).toStrictEqual({firstName : "Alex", lastName : "Ditto"})
 })
+
+test("Should equal 10", ()=>{
+    expect(functions.add(9,1)).toBe(10);
+})
